91d9961eabd36d219f8eea6a1cf320d84412e13f
NgWenJunFelix <felixng97@gmail.com>
2019-12-09 21:33:34 +0800

    Add SpawnerScript

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject currentBackground;
+    public GameObject backgroundToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(currentBackground.transform.position, new Vector3(0, 0, 0)) <= 0) {
+            Instantiate(backgroundToSpawn, transform.position, transform.rotation);
+        }
+    }
+}
