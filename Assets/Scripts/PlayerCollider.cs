c7a69e742fd2212358958a69329694778c8edcc6
NgWenJunFelix <felixng97@gmail.com>
2019-12-09 21:33:13 +0800

    Add PlayerCollider Script

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+
+    public PlayerMovement playerMovement;
+
+    
+    void OnTriggerEnter2D (Collider2D other) {
+
+        if (other.gameObject.tag == "Obstacle") {
+            playerMovement.enabled = false;
+        }
+
+    }
+
+}
