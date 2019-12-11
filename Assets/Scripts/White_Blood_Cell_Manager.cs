f585f2059ccf08c26db2f3012d64c748b698ae94
NgWenJunFelix <felixng97@gmail.com>
2019-12-11 23:22:24 +0900

    Added Sprite Renderer to WhiteBloodCell Prefab and script to destroy upon invisible to main camera

---
+++ Assets/Scripts/White_Blood_Cell_Manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class White_Blood_Cell_Manager : MonoBehaviour
+{
+    
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+
+}
