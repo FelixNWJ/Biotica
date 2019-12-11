ba57467e5a44a05defc170b68e2c765e3162ca07
NgWenJunFelix <felixng97@gmail.com>
2019-12-11 22:24:45 +0900

    Add PowerUpDoubleScore Script

---
+++ Assets/Scripts/PowerUpDoubleScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpDoubleScore : PowerUp
+{
+
+    public override IEnumerator executePowerUp() {
+
+        GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.scoreMultiplier = 2;
+
+        yield return new WaitForSeconds(10);
+
+        reversePowerUp(gameManager);
+    }
+
+    public override void reversePowerUp(GameManager gameManager) {
+
+        gameManager.scoreMultiplier = 1;
+
+    }
+
+}
