42643313338f79873ce10d572d0ce9485f1101ff
NgWenJunFelix <felixng97@gmail.com>
2019-12-11 13:19:57 +0900

    Update Spawner to move backgrounds instead of spawning, and add code to spawn Germ1

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,20 +5,49 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 
-    public GameObject currentBackground;
-    public GameObject backgroundToSpawn;
+    public GameObject backgroundToMoveNext;
+
+    public GameObject backgroundToMoveSecond;
+
+    public GameObject germToSpawn;
+
+    public float timeBetweenSpawningGerms;
+    private float timeLeftToSpawnGerms;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeftToSpawnGerms = timeBetweenSpawningGerms;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(currentBackground.transform.position, new Vector3(0, 0, 0)) <= 0) {
-            Instantiate(backgroundToSpawn, transform.position, transform.rotation);
+ 
+        if(backgroundToMoveNext.transform.position.x <= -87) {
+
+            backgroundToMoveNext.transform.position = new Vector3(143, backgroundToMoveNext.transform.position.y, 0);
+
+            GameObject temp = backgroundToMoveNext;
+            backgroundToMoveNext = backgroundToMoveSecond;
+            backgroundToMoveSecond = temp;
+        }
+
+        if(timeLeftToSpawnGerms <= 0) {
+            //Spawn Germ
+            float yCoordinateOfGerm = Random.Range(-10, 11);
+            Vector3 positionOfGermToSpawn = new Vector3(transform.position.x, yCoordinateOfGerm, 0);
+            Instantiate(germToSpawn, positionOfGermToSpawn, transform.rotation);
+
+
+            //Reset Timer
+            timeLeftToSpawnGerms = timeBetweenSpawningGerms;
+
+        } else {
+
+            //Reduce Timer
+            timeLeftToSpawnGerms -= Time.deltaTime;
+
         }
     }
 }
