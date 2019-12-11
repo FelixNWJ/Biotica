ee494fff1255348289dc70dc92869f49f5bb4f1b
NgWenJunFelix <felixng97@gmail.com>
2019-12-11 22:24:33 +0900

    Added base abstract class script for PowerUp

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUp : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    void FixedUpdate() {
+        transform.position += new Vector3(-1, 0, 0) * Time.fixedDeltaTime * moveSpeed;
+    }
+    public abstract IEnumerator executePowerUp();
+
+    public abstract void reversePowerUp(GameManager gameManager);
+
+}
