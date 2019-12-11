69c389d045717b82a34f523a325b0c5b2fea9540
NgWenJunFelix <felixng97@gmail.com>
2019-12-11 13:21:54 +0900

    Destroy Germ Objects when out of screen

---
+++ Assets/Scripts/GermManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GermManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+        Debug.Log("destroyed");
+    }
+
+}
