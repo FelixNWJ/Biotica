1c64fba047dfd0932b3be87b9f010a800e055c2d
NgWenJunFelix <felixng97@gmail.com>
2019-12-11 19:18:19 +0900

    added UI for collectible score and distance travelled

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject background;
+    private GameObject player;
+
+    private Text whiteBloodCellScoreUI;
+    private float whiteBloodCellScore = 0;
+
+    private Text distanceUI;
+    private float distance = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        whiteBloodCellScoreUI = GameObject.Find("White_Blood_Cell_Score").GetComponent<Text>();
+        whiteBloodCellScoreUI.text = "WBC: " + whiteBloodCellScore.ToString();
+
+        distanceUI = GameObject.Find("Distance").GetComponent<Text>(); 
+        distanceUI.text = "Distance: 0";
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        whiteBloodCellScoreUI.text = "WBC: " + whiteBloodCellScore.ToString();
+
+        distance += Time.deltaTime;
+        distanceUI.text = "Distance: " + distance.ToString();
+    }
+
+    public void increaseWhiteBloodCellScore() {
+        whiteBloodCellScore++;
     }
 }
