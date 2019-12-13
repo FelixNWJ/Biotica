c8597dcfea5eed782c2d8266d23530993c1d69fa
NgWenJunFelix <felixng97@gmail.com>
2019-12-13 22:04:05 +0900

    Load StartMenu scene when player collides with enemy

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollider : MonoBehaviour
 {
@@ -19,7 +20,7 @@ public class PlayerCollider : MonoBehaviour
     void OnTriggerEnter2D (Collider2D other) {
 
         if (other.gameObject.tag == "Enemy") {
-            playerMovement.enabled = false;
+            SceneManager.LoadScene("StartMenu");
         }
 
         if (other.gameObject.tag == "Background") {
