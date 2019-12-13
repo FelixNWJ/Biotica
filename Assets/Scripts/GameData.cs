8f69b7679f388dbb6cb7e3c8287761f833ad9beb
NgWenJunFelix <felixng97@gmail.com>
2019-12-13 23:00:56 +0900

    Add Template data to save, with saving and loading functionality

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    
+    float collectibleHighScore;
+
+    int distanceHighScore;
+
+    public GameData(GameManager game) {
+        this.collectibleHighScore = game.getWhiteBloodCellScore();
+        this.distanceHighScore = game.getDistance();
+    }
+
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    
+    public static void SaveScore(GameManager game) {
+
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string pathToSave = Application.persistentDataPath + "collectibleHighScore.data";
+
+        FileStream stream = new FileStream(pathToSave, FileMode.Create);
+
+        GameData data = new GameData(game); 
+        
+        formatter.Serialize(stream, data);
+
+        stream.Close();
+
+    }
+
+    public static GameData loadScore() {
+
+        string pathToLoad = Application.persistentDataPath + "collectibleHighScore.data";
+
+        try {
+
+            FileStream stream = new FileStream(pathToLoad, FileMode.Open);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return formatter.Deserialize(stream) as GameData;
+
+        } catch (FileNotFoundException e) {
+
+            Debug.LogError(e.Message);
+
+            return null;
+
+        }
+
+    }
+
+}
